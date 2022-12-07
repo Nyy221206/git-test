d39813485e7e52e7769fcc08f8f5792fd3e20ab0
NanYY <NanYY@cc.com>
2022-12-07 11:30:31 +0800

    idea first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @Author nyy
+ * @Data 2022/12/7 11:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Git Test！");
+    }
+}
